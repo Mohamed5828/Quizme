09336227b8a7889028bc9fb7afc7f4ef2e551097
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-09-29 04:23:09 +0300

    forgot to add files to prev commit

---
+++ frontend/src/utils/deleteData.ts
@@ -0,0 +1,42 @@
+import { AxiosError, isAxiosError, AxiosRequestConfig } from "axios";
+import { getAxiosInstance } from "./axiosInstance";
+
+interface DeleteDataResponse<T> {
+  resData: T | null;
+  loading: boolean;
+  error: AxiosError | null;
+}
+
+async function deleteData<T>(
+  url: string,
+  apiVersion: string = "v1",
+  headers: Record<string, string> = {}
+): Promise<DeleteDataResponse<T>> {
+  let loading = true;
+  let error: AxiosError | null = null;
+  let resData: T | null = null;
+
+  const axiosInstance = getAxiosInstance(apiVersion);
+
+  const config: AxiosRequestConfig = {
+    headers: { ...headers },
+  };
+
+  try {
+    const response = await axiosInstance.delete<T>(url, config);
+    resData = response.data;
+  } catch (e) {
+    if (isAxiosError(e)) {
+      error = e;
+      console.error("Error during request:", e.message);
+    } else {
+      error = new AxiosError("An unexpected error occurred");
+    }
+  } finally {
+    loading = false;
+  }
+
+  return { resData, loading, error };
+}
+
+export default deleteData;
---
+++ frontend/src/utils/putData.ts
@@ -0,0 +1,43 @@
+import { AxiosError, isAxiosError, AxiosRequestConfig } from "axios";
+import { getAxiosInstance } from "./axiosInstance";
+
+interface PutDataResponse<T> {
+  resData: T | null;
+  loading: boolean;
+  error: AxiosError | null;
+}
+
+async function putData<T>(
+  url: string,
+  data: any,
+  apiVersion: string = "v1",
+  headers: Record<string, string> = {}
+): Promise<PutDataResponse<T>> {
+  let loading = true;
+  let error: AxiosError | null = null;
+  let resData: T | null = null;
+
+  const axiosInstance = getAxiosInstance(apiVersion);
+
+  const config: AxiosRequestConfig = {
+    headers: { ...headers },
+  };
+
+  try {
+    const response = await axiosInstance.put<T>(url, data, config);
+    resData = response.data;
+  } catch (e) {
+    if (isAxiosError(e)) {
+      error = e;
+      console.error("Error during request:", e.message);
+    } else {
+      error = new AxiosError("An unexpected error occurred");
+    }
+  } finally {
+    loading = false;
+  }
+
+  return { resData, loading, error };
+}
+
+export default putData;
