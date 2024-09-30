16e2adf0ecf6405f498208c9970b12e45c201b61
KhaledHamouda <khaled7amoda@gmail.com>
2024-09-30 13:52:51 +0300

    Minor bugs solved

--- backend/exam_v2/urls.py
+++ backend/exam_v2/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import ExamViewSet, UserViewSet
+from .views import ExamViewSet
 
 router = DefaultRouter()
 router.register("", ExamViewSet)
