e3b802f526c38a23275c8fd005491111eb60f881
EslamRedaMohamed <eslamreda.info@gmail.com>
2024-10-06 13:58:09 +0300

    Navigate to home after logout and update profile page colors to align with site theme

--- frontend/src/components/Forms/ProfileForm.tsx
+++ frontend/src/components/Forms/ProfileForm.tsx
@@ -20,26 +20,26 @@ const ProfileForm = ({
     <div className="flex justify-center items-center min-h-screen bg-white-100">
       <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden -mt-40">
         <div className="md:flex">
-          <div className="md:w-1/3 bg-gradient-to-r from-pink-500 to-orange-400 p-6 flex flex-col justify-center items-center text-white">
+          <div className="md:w-1/3 bg-gradient-to-r from-emerald-500 to-white-400 p-6 flex flex-col justify-center items-center text-white">
             <img
               src="https://img.icons8.com/bubbles/100/000000/user.png"
               alt="User"
               className="rounded-full mb-4"
             />
-            <h6 className="text-xl font-semibold">{username || "Hembo Tingor"}</h6>
-            <p className="text-sm">{role || "Web Designer"}</p>
+            <h6 className="text-xl font-semibold">{username}</h6>
+            <p className="text-sm">{role || "Instructor"}</p>
           </div>
           <div className="md:w-2/3 p-8">
             <h6 className="text-2xl font-semibold mb-6">Information</h6>
             <div className="grid grid-cols-2 gap-y-8 gap-x-12">
               <div>
                 <p className="text-lg font-medium mb-2">Email</p>
-                <h6 className="text-gray-500">{email || "rntng@gmail.com"}</h6>
+                <h6 className="text-gray-500">{email || "Instructorg@gmail.com"}</h6>
               </div>
 
               <div>
                 <p className="text-lg font-medium mb-2">Role</p>
-                <h6 className="text-gray-500">{role || "Web Designer"}</h6>
+                <h6 className="text-gray-500">{role || "Instructor"}</h6>
               </div>
 
               <div className="col-span-2">
@@ -48,12 +48,12 @@ const ProfileForm = ({
 
               <div>
                 <p className="text-lg font-medium mb-2">Category</p>
-                <h6 className="text-gray-500">{category || "Software Development"}</h6>
+                <h6 className="text-gray-500">{category || "ITI"}</h6>
               </div>
 
               <div>
                 <p className="text-lg font-medium mb-2">Username</p>
-                <h6 className="text-gray-500">{username || "Hembo Tingor"}</h6>
+                <h6 className="text-gray-500">{username || "Instructor"}</h6>
               </div>
             </div>
 
