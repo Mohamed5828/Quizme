bab3194abc07545221bfd611bf012c30ecdc3db1
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-10-07 04:05:20 +0300

    Added check to prevent re-evaluation

--- backend/attempts/tasks.py
+++ backend/attempts/tasks.py
@@ -4,7 +4,7 @@ import csv
 from io import StringIO
 from django.core.mail import send_mail
 from django.conf import settings
-from exam.models import Exam 
+from exam.models import Exam
 from django.utils import timezone
 from django.core.mail import EmailMessage
 
@@ -13,25 +13,27 @@ from django.conf import settings
 from io import StringIO
 import csv
 
+
 @shared_task
 def send_attempts_csv(exam_id, instructor_email):
     attempts = Attempt.objects.filter(exam_id=exam_id).select_related('student_id')
     csv_file = StringIO()
     writer = csv.writer(csv_file)
-    
+
     writer.writerow(['Student ID', 'Student Email', 'Exam ID', 'Score', 'Start Time', 'End Time'])
 
     for attempt in attempts:
         student_email = attempt.student_id.email
-        writer.writerow([attempt.student_id.id, student_email, attempt.exam_id.id, attempt.score, attempt.start_time, attempt.end_time])
+        writer.writerow([attempt.student_id.id, student_email, attempt.exam_id.id, attempt.score, attempt.start_time,
+                         attempt.end_time])
 
     subject = f"Exam {exam_id} Attempts CSV"
     message = "Please find attached the CSV file containing all student attempts for the exam."
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [instructor_email]
-    
+
     csv_content = csv_file.getvalue()
-    
+
     # Prepare the email with an attachment
     email = EmailMessage(
         subject=subject,
@@ -39,25 +41,23 @@ def send_attempts_csv(exam_id, instructor_email):
         from_email=from_email,
         to=recipient_list,
     )
-    
+
     email.attach('exam_attempts.csv', csv_content, 'text/csv')
-    
+
     # Send the email
     email.send(fail_silently=False)
 
     return f'CSV file sent to {instructor_email}'
 
 
-
 @shared_task
 def check_exam_expiration():
-    
     # Fetch exams that have expired and haven't had their CSV sent
     expired_exams = Exam.objects.filter(expiration_date=timezone.now(), csv_sent=False)
-    
+
     for exam in expired_exams:
         # Queue the task to send the CSV to the instructor email
-        send_attempts_csv.delay(exam.id, exam.instructor.email)  
+        send_attempts_csv.delay(exam.id, exam.instructor.email)
         exam.csv_sent = True
         exam.save()
 
@@ -68,5 +68,5 @@ def evaluate_attempt(attempt_id):
     Evaluate an attempt
     """
     attempt = Attempt.objects.get(id=attempt_id)
-    attempt.evaluate()
-
+    if attempt.score is None:
+        attempt.evaluate()
