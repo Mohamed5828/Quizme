e9f480110624c0f9d484416f50e0904337d72fda
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-10-07 22:05:32 +0300

    small change

--- backend/exam_v2/tasks.py
+++ backend/exam_v2/tasks.py
@@ -3,19 +3,11 @@ from exam.models import Exam
 from attempts.tasks import evaluate_attempt
 from django.conf import settings
 from django.core.mail import send_mail
+from attempts.tasks import send_attempts_csv
 
 
 
 
-@shared_task
-def evaluate_exam_after_expiry(exam_id):
-    try:
-        exam = Exam.objects.get(id=exam_id)
-
-        for attempt in exam.attempts.all():
-            evaluate_attempt.delay(attempt.id)
-    except Exam.DoesNotExist:
-        print(f"Exam with {exam_id=} does not exist evaluation task will abort ")
 
 @shared_task
 def send_exam_invitation_email(email, exam_title, exam_code, start_date):
