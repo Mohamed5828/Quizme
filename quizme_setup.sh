0e5afbb2dabbc439a48356b7877ad5baae02602b
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-10-19 19:47:55 +0300

    made the script more verbose

--- quizme_setup.sh
+++ quizme_setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+set -e
+
+if [[ -d ./Quizme ]]; then
+  echo "Quizme directory already exists"
+  exit 1
+fi
+
+echo "Cloning the Quizme repository..."
+
 git clone https://github.com/Ahmed-Rushdi/Quizme
 cd Quizme
+
+
+echo "Setting up worktrees..."
 
 git worktree add --lock ./backend -b backend/main
 git worktree add --lock ./frontend -b frontend/main
@@ -16,6 +28,11 @@ git branch --set-upstream-to=origin/frontend/main frontend/main
 git reset --hard origin/frontend/main
 cd -
 
+
+echo "Starting Docker Compose..."
 docker compose up -d
 
+echo "Initializing Piston CLI..."
 /bin/bash init_piston.sh
+
+echo "Setup completed successfully."
