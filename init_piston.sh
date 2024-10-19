6d23165e419d6e629540a848fcd4eb52bb9decbc
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-10-19 19:40:12 +0300

    init setup for docker compose

---
+++ init_piston.sh
@@ -0,0 +1,8 @@
+# clone and enter repo
+git clone https://github.com/engineer-man/piston
+
+# Install all the dependencies for the cli
+cd cli && npm i && cd -
+
+#install python, sql, node, bash, java
+piston/cli/index.js ppman install python sql node bash java
---
+++ quizme_start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://github.com/EslamRedaMohamed/Quizme
+cd Quizme
+
+git worktree add --lock ./backend -b backend/main
+git worktree add --lock ./frontend -b frontend/main
+
+cd backend
+git branch --set-upstream-to=origin/backend/main backend/main 
+git reset --hard origin/backend/main
+cd -
+
+cd frontend
+git branch --set-upstream-to=origin/frontend/main frontend/main 
+git reset --hard origin/frontend/main
+cd -
+
+docker compose up -d
+
+/bin/bash init_piston.sh
