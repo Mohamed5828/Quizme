b4b4c2c2934731a2b62a7001e3a31678a69b1d2c
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-10-20 00:55:43 +0300

    made piston language installation more verbose and unexposed piston api

--- init_piston.sh
+++ init_piston.sh
@@ -4,5 +4,19 @@ git clone https://github.com/engineer-man/piston
 # Install all the dependencies for the cli
 cd piston/cli && npm i && cd -
 
+echo "Installing Piston language packages..."
 #install python, sql, node, bash, java
-piston/cli/index.js ppman install python sql node bash java
+echo "Installing the Python language packages..."
+piston/cli/index.js ppman install python 
+
+echo "Installing the SQL language packages..."
+piston/cli/index.js ppman install sqlite3
+
+echo "Installing the Node language packages..."
+piston/cli/index.js ppman install node
+
+echo "Installing the Bash language packages..."
+piston/cli/index.js ppman install bash
+
+echo "Installing the Java language packages..."
+piston/cli/index.js ppman install java
